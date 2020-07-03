8a85ae7efc9dce7d2eeb399bec2f52b8de966338
jshchn <jshchn@gmail.com>
2020-07-02 17:01:32 -0700

    create log-viewer

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,10 +1,11 @@
 const connection = new WebSocket("ws://localhost:3000")
 const logWindow = document.querySelector("#log-window")
+const logFilePath = document.getElementById("#logFilePath")
 
 connection.onopen = () => {
-    connection.send("Hello from the client!")
+    connection.send(filePath)
 };
 
 connection.onmessage = (event) => {
-    logWindow.innerHTML = event.data
+    logWindow.innerHTML = event.data.split("\n").join("<hr>")
 }
