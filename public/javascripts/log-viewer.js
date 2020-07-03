00314f7953c525ff59f48771cd1261f2d179b0c4
jshchn <jshchn@gmail.com>
2020-07-02 17:31:56 -0700

    create log-viewer

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,15 +1,13 @@
 const connection = new WebSocket("ws://localhost:3000")
 const logWindow = document.querySelector("#log-window")
-const filePath = document.getElementById("#logFilePath").value
+const filePath = document.getElementById("logFilePath").value
 
 connection.onopen = () => {
-    console.log("josh is within connection onopen" + filePath)
     if(filePath){
         connection.send(filePath)
     }
 };
 
 connection.onmessage = (event) => {
-    console.log("josh is within onmessage" + event.data.split("\n").join("<hr>"))
     logWindow.innerHTML = event.data.split("\n").join("<hr>")
 }
